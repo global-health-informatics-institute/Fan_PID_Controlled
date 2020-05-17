a3d3018fbcdd40423c7a0588c404e6819f513c0a
geraldpdouglas <geraldpdouglas@gmail.com>
2020-05-17 14:40:54 -0400

    COmment added by Gerry to test git commit

--- Wire_5_Sensor_Plus_LCD/Wire_5_Sensor_Plus_LCD.ino
+++ Wire_5_Sensor_Plus_LCD/Wire_5_Sensor_Plus_LCD.ino
@@ -3,6 +3,8 @@
 
 //LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
+// Gerry added this comments to test git push
+
 const int ADDR = 0x40;
 const int MeasureTemp = 0xE3;
 int X0,X1,temp;
